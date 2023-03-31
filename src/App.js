d4e44153dbde9a16b28f75558ed3ad2e99b61d41
MK5532 <121079522+MK5532@users.noreply.github.com>
2023-03-31 09:36:14 +0900

    교재 8. Props, state 사용하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
-import Hello from './Hello';
+import Todo from './Todo';
 
 class App extends React.Component{
+  constructor(props){ // 매개변수 props 생성자
+    super(props); // 매개변수 props 초기화
+    this.state = { // item에 item.id, item.title, item.done 매개변수 이름과 값 할당
+      item :{id:0, title:"Hello World 1 ", done:true},
+    };
+  }
+
   render(){
     return(
       <div className='App'>
-      <Hello />
+        <Todo item={this.state.item} />
     </div>
 
     );
