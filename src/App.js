c1b77ac8828aa7634cd27599379200941020438d
MK5532 <121079522+MK5532@users.noreply.github.com>
2023-04-05 15:22:13 +0900

    14. React + 백엔드 통합 ~ 16. 회원가입, 로그인, Todo 완성

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 import React from 'react';
 import Todo from './Todo';
 import AddTodo from './AddTodo';
-import { Paper, List, Container } from "@material-ui/core";
+import { Paper, List, Container, Grid, Button, AppBar, Toolbar, Typography } from "@material-ui/core";
 import './App.css';
+import { call, signout } from './service/ApiService';
 
 class App extends React.Component {
   constructor(props) { // 매개변수 props 생성자
     super(props); // 매개변수 props 초기화
     this.state = { // item에 item.id, item.title, item.done 매개변수 이름과 값 할당
-      items: [
-        { id: 0, title: "Todo 1 ", done: true },
-        { id: 1, title: "Todo 2 ", done: false },
-      ],
+      items: [],
+      /* 로딩 중이라는 상태를 표현할 변수 생성자에 상태 변수를 초기화한다 */
+      loading:true,
     };
   }
 
-  // add 함수추가
   add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length; // key를 위한 id 추가
-    item.done = false;
-    thisItems.push(item);
-    this.setState({ items: thisItems }); // update state
-    console.log("items", this.state.items)
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data })
+    );
   }
 
-  // delete 함수 추가
   delete = (item) => {
-    const thisItems = this.state.items;
-    const newItems = thisItems.filter(e => e.id !== item.id);
-    this.setState({ items: newItems }, () => {
-      // 디버깅 콜백
-      console.log("Update Items : ", this.state.items)
-    })
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
+
+  // componentDidMount는 페이즈(돔) 마운트가 일어나고 렌더링 되기 전에 실행된다.
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data, loading:false })
+    );
   }
 
   render() {
@@ -44,19 +49,51 @@ class App extends React.Component {
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
+            <Todo item={item} key={item.id} delete={this.delete} update={this.update}/>
           ))}
         </List>
       </Paper>
     );
 
+    var navigationBar = (
+      <AppBar position="static">
+        <Toolbar>
+          <Grid justifyContent="space-between" container>
+            <Grid item>
+              <Typography variant='h6'>오늘의 할일</Typography>
+            </Grid>
+            <Grid item>
+              <Button color="inherit" onClick={signout}>logout
+              </Button>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    );
+
+    //loading 중이 아닐 때
+    var todoListPage = (
+      <div>
+        {navigationBar}
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className='TodoList'>{todoItems}</div>
+        </Container>
+      </div>
+    );
+
+    // loading 중일 때
+    var loadingPage = <h1>로딩중..</h1>
+    var content = loadingPage;
+
+    if(!this.state.loading){
+      content = todoListPage;
+    }
+
     // 생성된 컴포넌트 JSX를 리턴한다.
     return (
       <div className="App">
-        <Container maxWidth="md">
-          <AddTodo add={this.add} />
-          <div className="TodoList">{todoItems}</div>
-        </Container>
+        {content}
       </div>
     )
   }
--- src/Todo.js
+++ src/Todo.js
@@ -7,6 +7,7 @@ class Todo extends React.Component {
         super(props);
         this.state = { item: props.item, readOnly: true }; // 매개변수 item의 변수/값을 item에 대입
         this.delete = props.delete;
+        this.update = props.update;
     }
 
     deleteEventHandler = () => {
@@ -14,7 +15,6 @@ class Todo extends React.Component {
     }
 
     offReadOnlyMode = () => {
-        console.log("Event!", this.state.readOnly);
         this.setState({ readOnly: false }, () => {
             console.log("ReadOnly?", this.state.readOnly)
         })
@@ -22,7 +22,8 @@ class Todo extends React.Component {
 
     enterKeyEventHandler = (e) => {
         if (e.key === "Enter") {
-            this.setState({ readOnly: true })
+            this.setState({ readOnly: true });
+            this.update(this.state.item);
         }
     }
 
@@ -33,10 +34,11 @@ class Todo extends React.Component {
     }
 
     checkboxEventHander = (e) => {
-        console.log("check box event call");
         const thisItem = this.state.item;
         thisItem.done = thisItem.done ? false : true; // thisItemdone = !thisitem.done
-        this.setState({ item: thisItem });
+        //this.setState({ item: thisItem });
+        this.setState({ readOnly: true });
+        this.update(this.state.item);
     }
 
     render() {
